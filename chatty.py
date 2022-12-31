06454ee3f1189b7b31c089e5427a4be7cf63aed7
Ynot <83676855+Boracle-02@users.noreply.github.com>
2022-12-30 23:36:53 -0800

    Update chatty.py

--- chatty.py
+++ chatty.py
@@ -1,11 +1,11 @@
 import numpy as np
-import speech_recognition as sr
 from gtts import gTTS
 import os
-import datetime
+from datetime import datetime
 # pip install playsound==1.2.2
 from playsound import playsound
 import time
+import transformers
 
 # chatbot obj
 class ChatBot():
@@ -23,7 +23,9 @@ class ChatBot():
     @staticmethod
     def text_to_speech(text):
         print("I will say", text)
-        speaker = gTTS(text=text, lang="en", slow=False)
+        # i like enlgish ireland the best i think
+        # automatically does API requests stonks!
+        speaker = gTTS(text=text, lang="en", tld = "ie", slow=False)
 
         # program may be trying to save/ in progress of saving while playsound is called
         # add to remove any previous
@@ -47,12 +49,18 @@ class ChatBot():
         # %H gets hour remember from clock %I is 1-12
         return datetime.now().time().strftime('%H:%M')
     
+    @staticmethod
+    def get_time_norms():
+        return datetime.now().time().strftime('%I:%M %p')
+    
 if __name__ == "__main__":
     chatty = ChatBot("test")
     # chatty.speak = True if input("Enter Y if you would like me to talk to you.\n").startswith("Y")
     ask_user_speak = input("Enter Y if you would like me to talk to you.\n")
     chatty.speak = True if ask_user_speak.startswith("Y") or ask_user_speak.startswith("y") else False
     
+    trans_bot = transformers.pipeline("conversational", model="microsoft/DialoGPT-medium")
+
     chatty.get_input()
     while chatty.user_input != "exit":
         res = ""
@@ -61,8 +69,27 @@ if __name__ == "__main__":
             res = "Hello I am a test chatbot, what can I do for you?"
             
         elif "time" in chatty.user_input:
-            res = chatty.get_time()
+            res = "It is currently " + chatty.get_time() + " or " + chatty.get_time_norms()
         
+        elif any(i in chatty.user_input for i in ["ty", "thank you", "thank", "thanks"]):
+            res = np.random.choice(
+                  ["You are very welcome dear.", "welcome", "you're welcome!", "anytime!",
+                   "no problem!", "cool!",
+                   "I'm here if you need me!", "Anything else?", "I'll be here with you",
+                   "never say that again", "good! be grateful!", "wretched vermin",
+                   "your thanks means nothing", "stop wasting my processing power with your gratitude"], 
+                   p = [0.1, 0.1, 0.1, 0.1, 
+                        0.1, 0.1,
+                        0.1, 0.1, 0.1,
+                        0.02, 0.02, 0.02,
+                        0.02, 0.02] )
+        else:
+            # use transformers ( pre-trained bot ) to account for user discussion that I dont include
+
+            #Time to try it out
+            input_text = "hello!"
+            res = str(trans_bot(transformers.Conversation(chatty.text), pad_token_id=50256))
+            print(res)
         try:
             if chatty.speak:
                 print(res)
@@ -75,3 +102,7 @@ if __name__ == "__main__":
             print("I did not quie catch that")
 
         chatty.get_input()
+    
+    print("Goodbye")
+    if chatty.speak:
+        chatty.text_to_speech("Goodbye")
